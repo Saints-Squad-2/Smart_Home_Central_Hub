a8180bee66dc49dad2b65a72d83d2772b7b23baf
bwilkins96 <ben.jr.speed@gmail.com>
2023-10-04 16:39:26 -0400

    added db to app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Express server for Smart Home Central Hub app
 
 const express = require('express');
+
+const knex = require('./database/knex/knex');
+const Base = require('./database/base');
 const SmartHomeApp = require('./classes/homeApp');
 
 function main() {
+    Base.knex(knex);
+
     const app = express();
     const port = 3000;
 
