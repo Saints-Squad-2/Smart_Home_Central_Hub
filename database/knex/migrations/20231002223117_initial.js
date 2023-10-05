b11e8f00e19931741425157484716e113873537d
bwilkins96 <ben.jr.speed@gmail.com>
2023-10-05 14:01:08 -0400

    migration update

--- database/knex/migrations/20231002223117_initial.js
+++ database/knex/migrations/20231002223117_initial.js
@@ -3,12 +3,24 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-    return knex.schema.createTable('appliances', table => {
-        table.increments('id').primary();
-        table.boolean('_poweredOn');
-        table.boolean('_connected');
-        table.string('_name');
-    });
+    return knex.schema
+        .createTable('appliances', table => {
+            table.increments('id').primary();
+            table.boolean('_poweredOn');
+            table.boolean('_connected');
+            table.string('_name');
+        })
+        .createTable('notifications', table => {
+            table.increments('id').primary();
+            table.string('_info');
+            table.boolean('_active');
+            table.integer('_notArrId');
+        })
+        .createTable('notificationArrays', table => {
+            table.increments('id').primary();
+            table.integer('_applianceId');
+            table.boolean('_show');
+        });
 };
 
 /**
@@ -16,5 +28,8 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-    return knex.schema.dropTable('appliances');
+    return knex.schema
+        .dropTable('appliances')
+        .dropTable('notifications')
+        .dropTable('notificationArrays');
 };
