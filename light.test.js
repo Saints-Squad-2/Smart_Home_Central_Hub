38ce1089c0c3fa9ff136b0d7f389b7b257ec968a
dondon-89 <133548646+dondon-89@users.noreply.github.com>
2023-10-13 15:34:19 -0500

    Add files via upload

---
+++ light.test.js
@@ -0,0 +1,76 @@
+const { Light } = require('./light');
+
+describe('Light', () => {
+  let light;
+
+  beforeEach(() => {
+    light = new Light('Living Room Light', 75);
+  });
+
+  test('Light can be turned on', () => {
+    light.turnOn();
+    expect(light.poweredOn).toBe(true);
+  });
+
+  test('Light can be turned off', () => {
+    light.turnOff();
+    expect(light.poweredOn).toBe(false);
+  });
+
+  test('Brightness can be set', () => {
+    light.brightness = 50;
+    expect(light.brightness).toBe(50);
+  });
+
+  test('Enable voice control', () => {
+    light.enableVoiceControl();
+    expect(light._voiceControlEnabled).toBe(true);
+  });
+
+  test('Disable voice control', () => {
+    light.disableVoiceControl();
+    expect(light._voiceControlEnabled).toBe(false);
+  });
+
+  test('Enable motion detection', () => {
+    light.enableMotionDetection();
+    expect(light._motionDetectionEnabled).toBe(true);
+  });
+
+  test('Disable motion detection', () => {
+    light.disableMotionDetection();
+    expect(light._motionDetectionEnabled).toBe(false);
+  });
+
+  test('Respond to voice command when voice control is enabled', () => {
+    light.enableVoiceControl();
+    const command = 'Turn on the light';
+    const mockConsoleLog = jest.spyOn(console, 'log');
+    light.respondToVoiceCommand(command);
+    expect(mockConsoleLog).toHaveBeenCalledWith(`Received voice command: "${command}"`);
+  });
+
+  test('Do not respond to voice command when voice control is disabled', () => {
+    light.disableVoiceControl();
+    const command = 'Turn on the light';
+    const mockConsoleLog = jest.spyOn(console, 'log');
+    light.respondToVoiceCommand(command);
+    expect(mockConsoleLog).toHaveBeenCalledWith('Voice control is not enabled for this light.');
+  });
+
+  test('Detect motion and turn on the light when motion detection is enabled', () => {
+    light.enableMotionDetection();
+    const mockConsoleLog = jest.spyOn(console, 'log');
+    light.detectMotion();
+    expect(mockConsoleLog).toHaveBeenCalledWith('Motion detected; turning on the light.');
+    expect(light.poweredOn).toBe(true);
+  });
+
+  test('Do not detect motion and turn on the light when motion detection is disabled', () => {
+    light.disableMotionDetection();
+    const mockConsoleLog = jest.spyOn(console, 'log');
+    light.detectMotion();
+    expect(mockConsoleLog).toHaveBeenCalledWith('Motion detection is not enabled for this light.');
+    expect(light.poweredOn).toBe(false);
+  });
+});
---
+++ light_class.js
@@ -0,0 +1,70 @@
+const { SmartAppliance } = require('./smartAppliance');
+
+class Light extends SmartAppliance {
+  constructor(name = '', brightness = 100) {
+    super(name);
+    this._brightness = brightness;
+    this._voiceControlEnabled = false;
+    this._motionDetectionEnabled = false;
+  }
+
+  get brightness() {
+    return this._brightness;
+  }
+
+  set brightness(value) {
+    if (value >= 0 && value <= 100) {
+      this._brightness = value;
+      console.log(`Set brightness to ${value}%`);
+    } else {
+      console.log('Invalid brightness value. It should be between 0 and 100.');
+    }
+  }
+
+  turnOn() {
+    console.log('Light is turned on.');
+  }
+
+  turnOff() {
+    console.log('Light is turned off.');
+  }
+
+  enableVoiceControl() {
+    this._voiceControlEnabled = true;
+    console.log('Voice control for the light is now enabled.');
+  }
+
+  disableVoiceControl() {
+    this._voiceControlEnabled = false;
+    console.log('Voice control for the light is now disabled.');
+  }
+
+  enableMotionDetection() {
+    this._motionDetectionEnabled = true;
+    console.log('Motion detection for the light is now enabled.');
+  }
+
+  disableMotionDetection() {
+    this._motionDetectionEnabled = false;
+    console.log('Motion detection for the light is now disabled.');
+  }
+
+  respondToVoiceCommand(command) {
+    if (this._voiceControlEnabled) {
+      console.log(`Received voice command: "${command}"`);
+    } else {
+      console.log('Voice control is not enabled for this light.');
+    }
+  }
+
+  detectMotion() {
+    if (this._motionDetectionEnabled) {
+      console.log('Motion detected; turning on the light.');
+      this.turnOn();
+    } else {
+      console.log('Motion detection is not enabled for this light.');
+    }
+  }
+}
+
+module.exports = { Light };
