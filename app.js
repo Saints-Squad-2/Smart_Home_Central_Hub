6f95861c2c77f6e9da55eabd5f567330fd5dda5a
bwilkins96 <ben.jr.speed@gmail.com>
2023-10-19 17:36:12 -0400

    expanded server

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
 // Express server for Smart Home Central Hub app
 
 const express = require('express');
+const path = require('path');
 
 const knex = require('./database/knex/knex');
 const Base = require('./database/base');
 const SmartHomeApp = require('./classes/homeApp');
 
+const { Notification, NotificationArray } = require('./classes/notifications');
+const { SmartAppliance } = require('./classes/appliance');
+const { Thermostat } = require('./classes/thermostat');
+const { Camera } = require('./classes/camera');
+const { Light } = require('./classes/light/light');
+
 function main() {
     Base.knex(knex);
+
+    const smartHomeApp = new SmartHomeApp();
 
     const app = express();
     const port = 3000;
 
     app.get('/', (req, res) => {
-        res.send('Hello from the Smart Home Central Hub!');
+        return res.send('Hello from the Smart Home Central Hub!');
+    });
+
+    app.get('/speechToText', (req, res) => {
+        return res.sendFile(path.join(__dirname, '/speechToText/speech-to-txt.html'));
+    });
+
+    app.get('/speechToText.js', (req, res) => {
+        return res.sendFile(path.join(__dirname, '/speechToText/speechToText.js'));
+    })
+
+    app.get('/app', (req, res) => {
+        return res.json(smartHomeApp);
+    });
+
+    app.get('/app/add/:type', (req, res) => {
+        const type = req.params.type.toLowerCase();
+        let newItemClass;
+        
+        switch (type) {
+            case 'thermostat':
+                newItemClass = Thermostat;
+                break;
+            case 'camera':
+                newItemClass = Camera;
+                break;
+            case 'light':
+                newItemClass = Light;
+                break;
+        }
+
+        if (newItemClass) {
+            smartHomeApp.addAppliance(new newItemClass(new NotificationArray()));
+            return res.redirect('/app');
+        }
+        
+        return res.send('Invalid SmartAppliance Type');
     });
 
     app.listen(port, () => {
