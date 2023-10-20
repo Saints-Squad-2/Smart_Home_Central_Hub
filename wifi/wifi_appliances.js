21a5dcaa7767105767f9384d0ef00d37b5d0c11f
bwilkins96 <ben.jr.speed@gmail.com>
2023-10-20 17:02:25 -0400

    integrated wifi code

--- wifi/wifi_appliances.js
+++ wifi/wifi_appliances.js
@@ -1,16 +1,18 @@
 const axios = require('axios');
 
+const smartHomeHubIp = 'localhost';
+
 const applianceData = {
   id: 'appliance123',
   type: 'light',
   location: 'living room',
 };
 
-axios.post('http://your-smart-home-hub-ip:3000/connect', applianceData)
+axios.post(`http://${smartHomeHubIp}:3000/connect`, applianceData)
   .then(response => {
     console.log(response.data);
     // Handle successful connection
   })
   .catch(error => {
-    console.error('Error connecting to the smart home hub:', error);
+    console.error('Error connecting to the smart home hub:\n\n', error);
   });
